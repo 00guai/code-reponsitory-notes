79599ad5a0e094d2e654998ceefd235ceab526d2
00guai <1986055395@qq.com>
2021-11-12 23:00:54 +0800

    提交代码

---
+++ chengfa/源.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+	int x = 1, y = 10, z = 20, n = 0;
+	switch (x):x==
+	{
+	case 1:
+		switch (y);
+		{
+		case 10:
+			n++;
+			break;
+		case 11 :
+			n++;
+			break;
+		}
+	case 2:
+		switch (z);
+		{
+		case 20 :
+			n++;
+			break;
+		case 21:
+			n++;
+			break:
+		}
+		break;
+	case 3 :
+		n++;
+		break;
+		break;
+	};
+
+}
