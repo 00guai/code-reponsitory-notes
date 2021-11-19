d30375e3d8c55a90bf26828f0df8f8429f27128f
00guai <1986055395@qq.com>
2021-11-19 12:51:52 +0800

    Enter password game test

---
+++ 输入密码游戏测试/源.cpp
@@ -0,0 +1,28 @@
+#define   _CRT_SECURE_NO_WARNINGS  1
+
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+#include<stdlib.h>
+int main()
+{
+	int i = 0;
+	char password[10] = { 0 };
+	for (i = 0; i < 3; i++)
+	{
+		printf("请输入密码：");
+		scanf("%s", password);
+		if(strcmp(password, "00000000") == 0)//strcmp必须用string文件
+			{
+				printf("密码正确，登录成功！\n");
+				break;
+			}
+		else
+		{
+			printf("密码错误\n");
+		}
+	}
+	if (i == 3)
+		printf("三次密码均错误，请退出程序\n");
+	return 0;
+ } 
