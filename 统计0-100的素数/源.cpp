379eb124c3efaa72485cd85f82c350c945a0467b
00guai <1986055395@qq.com>
2021-11-25 09:42:22 +0800

    Statistics 0-100 prime numbers

---
+++ 统计0-100的素数/源.cpp
@@ -0,0 +1,24 @@
+#define   _CRT_SECURE_NO_WARNINGS  1
+
+#include<stdio.h>
+#include<math.h>
+int is_prime(int n)
+{
+	int j = 0;
+	for (j = 2; j <= sqrt(n); j++)
+	{
+		if (n%j == 0)
+			return 0;
+	}
+	return 1;
+}
+int main()
+{
+	int i = 0;
+	for (i = 0; i <= 100; i++)
+	{
+		if (is_prime(i) == 1)
+			printf("%d\n", i);
+	}
+	return 0;
+}
